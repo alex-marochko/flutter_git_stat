5877fd27523ef8928eca363b6b76b6991cccdb29
O.Marochko <omarochko@star.global>
2023-05-28 17:25:07 +0300

    experiment with ExpansionTile (don't like it)

--- lib/releases/view/releases_page.dart
+++ lib/releases/view/releases_page.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:git_stat/releases/presenter/releases_cubit.dart';
@@ -21,41 +19,81 @@ class ReleasesPage extends StatelessWidget {
           ReleasesStatus.initial ||
           ReleasesStatus.loading =>
             const CircularProgressIndicator(),
-          ReleasesStatus.success => ListView.separated(
-              itemCount: state.repos.length,
-              itemBuilder: (context, id) {
-                return ListTile(
-                  enableFeedback: true,
-                  onTap: () {
-                    log('hey');
-                  },
-                  onLongPress: () {
-                    log('hey');
-                  },
-                  leading: CircleAvatar(
-                    backgroundImage:
-                        NetworkImage(state.repos[id].ownerAvatarUrl),
-                    // child: Image.network(state.repos[id].ownerAvatarUrl),
-                  ),
-                  title: Text(state.repos[id].nameWithOwner),
-                  subtitle: Text(state.repos[id].url),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) {
-                return Container(
-                  width: double.infinity,
-                  height: 1,
-                  margin: const EdgeInsets.only(left: 64),
-                  decoration: const ShapeDecoration(
-                    shape: LinearBorder(),
-                    color: Color(0xFFDDDDDD),
-                  ),
-                );
-              },
+          ReleasesStatus.success => ReleasesList(
+              state: state,
             ),
           _ => const Center(child: Text('Oops'))
         };
       }),
+    );
+  }
+}
+
+class ReleasesList extends StatelessWidget {
+  const ReleasesList({
+    super.key,
+    required this.state,
+  });
+
+  final ReleasesState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: state.repos.length,
+      itemBuilder: (context, i) {
+        return ExpansionTile(
+          key: PageStorageKey(state.repos[i].id),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(state.repos[i].ownerAvatarUrl),
+          ),
+          title: Text(state.repos[i].nameWithOwner),
+          subtitle: Text(
+            state.repos[i].description ?? '',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(color: Colors.grey),
+          ),
+          trailing: Text('⭐${state.repos[i].stargazersCount}'),
+          maintainState: true,
+          children: [
+            ...state.repos[i].releases.map((r) => ListTile(
+                  subtitle: Text(
+                    r.toString(),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                )),
+          ],
+        );
+
+        /*ListTile(
+          enableFeedback: true,
+          onTap: () {
+            log('hey');
+          },
+          onLongPress: () {
+            log('hey');
+          },
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(state.repos[id].ownerAvatarUrl),
+            // child: Image.network(state.repos[id].ownerAvatarUrl),
+          ),
+          title: Text(state.repos[id].nameWithOwner),
+          subtitle: Text(state.repos[id].url),
+        );*/
+      },
+/*      separatorBuilder: (BuildContext context, int index) {
+        return Container(
+          width: double.infinity,
+          height: 1,
+          margin: const EdgeInsets.only(left: 64),
+          decoration: const ShapeDecoration(
+            shape: LinearBorder(),
+            color: Color(0xFFDDDDDD),
+          ),
+        );
+      },*/
     );
   }
 }
